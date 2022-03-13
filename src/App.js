3885b57756488412fe4e274aba3702461c76b311
Gregory Caloin <gregchapauli@gmail.com>
2022-03-13 15:26:04 +0100

    Connexion du "store" dans App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PhoneComponent from "./components/PhoneComponent";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="section-one">
-      <PhoneComponent />
-    </div>
+    <Provider store={store}>
+      <div className="section-one">
+        <PhoneComponent />
+      </div>
+    </Provider>
   );
 }
 

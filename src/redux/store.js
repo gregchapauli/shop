9a6987d16f48ee0e317b450a4f4ebd6ca1d2093d
Gregory Caloin <gregchapauli@gmail.com>
2022-03-15 11:23:11 +0100

    Inclusion du comment Reducer dans le store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from "redux";
 import phoneReducer from "./Phone/reducerPhone";
 import tvReducer from "./tv/reducerTv";
+import commentReducer from "./comments/reducerComments";
 
 const rootReducer = combineReducers({
   phone: phoneReducer,
   television: tvReducer,
+  comments: commentReducer,
 });
 
 const store = createStore(rootReducer);

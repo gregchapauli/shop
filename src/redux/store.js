4d32cb71df710d3139efccb2eb6416795c3e1684
Gregory Caloin <gregchapauli@gmail.com>
2022-03-15 11:29:56 +0100

    Mise en place du middleware dans le store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import phoneReducer from "./Phone/reducerPhone";
 import tvReducer from "./tv/reducerTv";
 import commentReducer from "./comments/reducerComments";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   phone: phoneReducer,
@@ -9,6 +10,6 @@ const rootReducer = combineReducers({
   comments: commentReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;

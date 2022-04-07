0369cca7771b0dc79d176fdb4dde18b366ac3ef0
Gregory Caloin <gregchapauli@gmail.com>
2022-04-07 18:35:24 +0200

    Stylisation du shop

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 function App() {
   return (
     <Provider store={store}>
+      <h1>Welcome to my Shop</h1>
+      <hr />
       <div className="section-one">
         <PhoneContainer />
         <TvContainer />
